73ff2637b12dc52529b253d4261cc6169fd5fa64
Aphelele2 <aphelelejoyi@gmail.com>
2023-05-22 15:39:48 +0200

    dot

--- script/code.js
+++ script/code.js
@@ -1,39 +1,147 @@
+let cnt = 0;
+// let num = 0;
 let operator = "";
-let firstOp = 0;
+let results = document.querySelector('#output');
+
+
 // display values on form
-let result = document.querySelector('.display');
+let result = document.querySelector('.show');
 function display(value) {
     result.value += value;
 } 
 
 
-
+//clear everything on form
 
 function clear() {
     result.value = "";
 }
-
-// Clear button
-document.querySelector('#clear')
-.addEventListener('click', ()=>{
+document.querySelector('#clear').addEventListener('click', ()=>{
     clear();
 })
 
 
 // backspace
-document.querySelector('#del').
-addEventListener('click', ()=>{
+document.querySelector('#del').addEventListener('click', ()=>{
     let output = result.value;
     result.value = output.slice(0, -1);
 })
 
 
-// equal
-document.querySelector('#division')
-.addEventListener('click', ()=>{
+
+
+
+//Division
+document.querySelector('#equal').addEventListener('click', ()=>{
+    let num2 = result.value;
+    switch(operator){
+        case '/':
+            result.value = 
+            eval(`${num}/${num2}`).toFixed(2)
+        break;
+    }
+})
+
+
+
+document.querySelector('#division').addEventListener('click', ()=>{
     firstOp = result.value;
     operator = "/";
     clear();
 })
 
+//Addition
 
+document.querySelector('#equal').addEventListener('click', ()=>{
+    let num2 = result.value;
+    switch(operator){
+        case '+':
+            result.value = 
+            eval(`${num}+${num2}`).toFixed(2)
+            break;
+        }
+})
+
+
+
+document.querySelector('#addition').addEventListener('click', ()=>{
+    num = result.value;
+    operator = "+";
+    clear();
+})
+
+
+
+
+//Subtraction
+
+document.querySelector('#equal').addEventListener('click', ()=>{
+    let num2 = result.value;
+    switch(operator){
+        case '-':
+            result.value = 
+            eval(`${num}-${num2}`).toFixed(2)
+        break;
+    }
+})
+
+
+
+document.querySelector('#subtraction').addEventListener('click', ()=>{
+    num = result.value;
+    operator = "-";
+    clear();
+})
+
+
+
+//Multiplication
+
+document.querySelector('#equal').addEventListener('click', ()=>{
+    let num2 = result.value;
+    switch(operator){
+        case '*':
+            result.value = 
+            eval(`${num}*${num2}`).toFixed(2)
+            break;
+        }
+    })
+    
+    
+    
+document.querySelector('#multiplication').addEventListener('click', ()=>{
+        num = result.value;
+        operator = "*";
+        clear();
+    })
+    
+
+// modulus
+
+document.querySelector('#equal').addEventListener('click', ()=>{
+    let num2 = result.value;
+    switch(operator){
+        case '%':
+            result.value = 
+            eval(`${num}%${num2}`).toFixed(2)
+            break;
+        }
+    })
+    
+    
+    
+document.querySelector('#mod').addEventListener('click', ()=>{
+    num = result.value;
+    operator = "%";
+    clear();
+})
+
+document.querySelector('#dot').addEventListener('click', ()=>{
+    if (cnt>=1){
+    document.querySelector('#dot').disabled = true;
+    }else {
+        display('.');
+    }
+    cnt++;
+
+})
